0b527a54fdf87388f4fb5249bca811b0e514c82f
sc30gsw <haidoushi875@gmail.com>
2023-05-02 15:15:41 +0900

    DB接続設定

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 const PORT = 4000;
+const url = process.env.MONGODB_URL as string;
 
-app.get("/", (req, res) => {
-	res.send("Hello, MERN with TypeScript!");
-});
+app.use(express.json());
+
+// DB接続
+try {
+	mongoose.set("strictQuery", true);
+	mongoose.connect(url);
+	console.log("DB接続完了");
+} catch (err) {
+	console.log(err);
+}
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
