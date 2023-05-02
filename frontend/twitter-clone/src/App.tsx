9937155e0788e97887a6e54b71d97872826015dc
sc30gsw <haidoushi875@gmail.com>
2023-05-03 08:06:21 +0900

    認証ページのルーティング設定

--- frontend/twitter-clone/src/App.tsx
+++ frontend/twitter-clone/src/App.tsx
@@ -1,5 +1,41 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/layout/AuthLayout";
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
+
 const App = () => {
-	return <div></div>;
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main: "#1DA1F2",
+			},
+			secondary: {
+				main: "#657786",
+			},
+			background: {
+				default: "#FFFFFF",
+			},
+			text: {
+				primary: "#14171A",
+				secondary: "#657786",
+			},
+		},
+	});
+
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<AuthLayout />}>
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
+	);
 };
 
 export default App;
