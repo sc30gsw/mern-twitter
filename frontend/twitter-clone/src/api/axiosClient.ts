b411966bdf2cd7db96d862663f24755bb73a5bf8
sc30gsw <haidoushi875@gmail.com>
2023-05-03 11:49:11 +0900

    API（バックエンド）との接続設定

---
+++ frontend/twitter-clone/src/api/axiosClient.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL as string;
+console.log(BASE_URL);
+
+const getToken = () => localStorage.getItem("token");
+
+export const axiosClient = axios.create({
+	baseURL: BASE_URL,
+});
+
+axiosClient.interceptors.request.use(async (config: any) => {
+	const token = await getToken();
+	return {
+		...config,
+		headers: {
+			"Content-Type": "application/json",
+			authorization: `Bearer ${token}`,
+		},
+	};
+});
+
+axiosClient.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	(err) => {
+		throw err.response;
+	}
+);
