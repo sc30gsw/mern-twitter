1c0738a6c57d9649a769b0824b34ad69b72b2739
sc30gsw <haidoushi875@gmail.com>
2023-05-05 17:27:22 +0900

    パスワード再設定処理のAPIを定義

--- frontend/twitter-clone/src/api/authApi.ts
+++ frontend/twitter-clone/src/api/authApi.ts
@@ -9,6 +9,18 @@ const authApi = {
 	// JWT認証API
 	verifyToken: (token: string) =>
 		axiosClient.post("auth/verify-token", { token }),
+	// パスワード忘れリクエストAPI
+	forgotPassword: (param: {
+		username: string | undefined;
+		email: string | undefined;
+	}) => axiosClient.post("auth/forgotPassword", param),
+	// パスワードリセットAPI
+	resetPassword: (param: {
+		username: string | undefined;
+		email: string | undefined;
+		password: string;
+		confirmPassword: string;
+	}) => axiosClient.post("auth/resetPassword", param),
 };
 
 export default authApi;
