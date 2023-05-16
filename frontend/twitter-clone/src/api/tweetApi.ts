3f2bc7a525c70bfb659b2aae1c404360cfd00424
sc30gsw <haidoushi875@gmail.com>
2023-05-16 21:34:07 +0900

    リツイート作成・削除APIのフロント定義

--- frontend/twitter-clone/src/api/tweetApi.ts
+++ frontend/twitter-clone/src/api/tweetApi.ts
@@ -13,6 +13,12 @@ const tweetApi = {
 	// ユーザーツイート一覧取得API
 	searchUserTweets: (username: string) =>
 		axiosClient.post("tweet/searchUserTweets", { username }),
+	// リツイート作成API
+	createRetweet: (params: { userId: string; tweetId: string }) =>
+		axiosClient.post("tweet/createRetweet", params),
+	// リツイート削除API
+	deleteRetweet: (tweetId: string) =>
+		axiosClient.delete("tweet/deleteRetweet", { params: { tweetId: tweetId } }),
 };
 
 export default tweetApi;
