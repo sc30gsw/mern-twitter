b6644efb7b0745014c485ea6fcf57390f1449eba
sc30gsw <haidoushi875@gmail.com>
2023-05-16 21:34:41 +0900

    リツイートの型定義を追加

--- frontend/twitter-clone/src/types/Tweet.ts
+++ frontend/twitter-clone/src/types/Tweet.ts
@@ -1,10 +1,20 @@
 import { TweetUser } from "./User";
 
+type Retweet = {
+	originalTweetId: string;
+	originalUser: TweetUser;
+	originalContent: string;
+	originalTweetImage: string[];
+	originalCreatedAt: Date;
+	originalUpdatedAt: Date;
+};
+
 export type Tweet = {
 	_id: string;
 	userId: string;
 	content: string;
 	tweetImage: string[];
+	retweet: Retweet;
 	createdAt: Date;
 	updatedAt: Date;
 	__v: number;
